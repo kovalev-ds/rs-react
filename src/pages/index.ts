fcee35abc6539249158b4a05148333ded058df8c
kovvdn <40830091+kovvdn@users.noreply.github.com>
2023-03-20 20:27:32 +0300

    feat: init form page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as Home } from './home/Home';
 export { default as About } from './about/About';
+export { default as Form } from './form/Form';
 export { default as NotFound } from './not-found/NotFound';
