346ac424342b07b0d1198c086f0b2e22ad707917
kovvdn <40830091+kovvdn@users.noreply.github.com>
2023-03-30 22:08:10 +0300

    refactor: rewrite to hooks

--- src/pages/events/Events.tsx
+++ src/pages/events/Events.tsx
@@ -1,62 +1,47 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
 
 import { List, Toast } from '~/components';
 import { EventForm, EventCard, Event, EventEmptyCard } from '~/features/events';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface EventsProps {}
+const Events: FC = () => {
+  const [isToastShown, setIsToastShown] = useState<boolean>(false);
+  const [events, setEvents] = useState<Event[]>([]);
 
-interface EventsState {
-  events: Event[];
-  isToastShown: boolean;
-}
-
-class Events extends React.Component<EventsProps, EventsState> {
-  constructor(props: EventsProps) {
-    super(props);
-    this.state = { isToastShown: false, events: [] };
-  }
-
-  addEvent = (event: Event) => {
-    this.setState((state) => ({ ...state, events: [...state.events, event] }));
-    this.showToast();
+  const addEvent = (event: Event) => {
+    setEvents((prev) => [...prev, event]);
+    showToast();
   };
 
-  showToast = () => {
-    this.setState((state) => ({ ...state, isToastShown: true }));
+  const showToast = () => {
+    setIsToastShown(() => true);
 
     setTimeout(() => {
-      this.setState((state) => ({ ...state, isToastShown: false }));
+      setIsToastShown(() => false);
     }, 5000);
   };
 
-  render() {
-    return (
-      <div className="relative overflow-hidden">
-        <div
-          className={`absolute z-50 right-0 top-0 transition-transform duration-300 ${
-            this.state.isToastShown ? 'translate-x-0' : 'translate-x-[150%]'
-          }`}
-        >
-          <Toast content="The event was successfully created!" />
-        </div>
-
-        <div className="grid grid-cols-[4fr,7fr] gap-x-6">
-          <EventForm onSubmit={this.addEvent} />
-          {this.state.events.length ? (
-            <div className="grid grid-cols-3 auto-rows-min gap-2">
-              <List
-                items={this.state.events}
-                fn={(item) => <EventCard key={item.id} item={item} />}
-              />
-            </div>
-          ) : (
-            <EventEmptyCard />
-          )}
-        </div>
+  return (
+    <div className="relative overflow-hidden">
+      <div
+        className={`absolute z-50 right-0 top-0 transition-transform duration-300 ${
+          isToastShown ? 'translate-x-0' : 'translate-x-[150%]'
+        }`}
+      >
+        <Toast content="The event was successfully created!" />
       </div>
-    );
-  }
-}
+
+      <div className="grid grid-cols-[4fr,7fr] gap-x-6">
+        <EventForm onSubmit={addEvent} />
+        {events.length ? (
+          <div className="grid grid-cols-3 auto-rows-min gap-2">
+            <List items={events} fn={(item) => <EventCard key={item.id} item={item} />} />
+          </div>
+        ) : (
+          <EventEmptyCard />
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Events;
